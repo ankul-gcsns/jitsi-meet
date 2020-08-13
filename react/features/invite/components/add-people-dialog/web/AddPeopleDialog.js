fb1e6a806b6abe91a23f33eccf729c1fdd3dd613
Ankul Kumar <ankul.kumar@gamechangesns.com>
2020-08-13 16:13:07 +0530

    feat: update add more people dialog box

--- react/features/invite/components/add-people-dialog/web/AddPeopleDialog.js
+++ react/features/invite/components/add-people-dialog/web/AddPeopleDialog.js
@@ -149,10 +149,6 @@ function AddPeopleDialog({
             <div className = 'invite-more-dialog'>
                 { _inviteContactsVisible && <InviteContactsSection /> }
                 <CopyMeetingLinkSection url = { _inviteUrl } />
-                <InviteByEmailSection
-                    inviteSubject = { inviteSubject }
-                    inviteText = { invite } />
-                <EmbedMeetingTrigger />
                 <div className = 'invite-more-dialog separator' />
                 {
                     _liveStreamViewURL
